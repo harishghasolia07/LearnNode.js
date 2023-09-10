b9019dda02575003381da990896a614711012d8b
harishghasolia07 <100846446+harishghasolia07@users.noreply.github.com>
2023-09-10 13:48:17 +0530

    Create index.js

---
+++ NPM/index.js
@@ -0,0 +1,16 @@
+// // var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require('superheroes');
+
+import superheroes from "superheroes";
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+var name = superheroes.random();
+
+console.log(`My name is ${name}!`);
